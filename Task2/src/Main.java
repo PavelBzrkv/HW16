53b315a330e9c8611d1065b1a9114f61886a4055
PavelBzrkv <pavel95959@gmail.com>
2025-05-05 16:17:19 +0500

    Regex task2

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //код
-        textOneHundredWords("Most: people :: would---- agree that -----" +
+        String myText = "Most: people :: would---- agree that -----" +
                 " 6747, it is great, to have; a pet." +
                 " They are our friends, companions, family members 344." +
                 " I have an adorable calico cat as a pet, her name is Minnie." +
@@ -11,11 +11,12 @@ public class Main {
                 " A calico cat is usually white with black and orange patches on the coat." +
                 " Minnie is very curious and likes to walk outdoors." +
                 " We live on the ground floor, " +
-                "so she just jumps out of the window when she wants to take a walk 989 995432.");
+                "so she just jumps out of the window when she wants to take a walk 989 995432.";
+
+        textOneHundredWords(myText);
 
     }
     // метод
-
     private static void textOneHundredWords(String text){
         //String[] str = text.split("[\\s,.:-]+[0-9]+");
         String[] str = text.split("[\\s|\\d|\\-|,|;|:|\\.]+");
